c264bd4ef15aabd77290397221f2cbbda3880914
Ahmed-AAY <162641927+AhmedA-AY@users.noreply.github.com>
2025-03-04 17:40:04 +0300

    Update error.js

--- app/error.js
+++ app/error.js
@@ -3,6 +3,14 @@ import React from 'react'
 export default function Error({ statusCode }) {
   return (
     <div className="error-message">
+      <style jsx>{`
+        .error-message {
+          color: red;
+          font-size: 20px;
+          text-align: center;
+          margin-top: 20px;
+        }
+      `}</style>
       <p>
         {statusCode
           ? `An error ${statusCode} occurred on server`
